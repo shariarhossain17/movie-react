4de0fb75e9437f8d86c4b953d8cb2269ce5db0bf
shariarhossain23e <shariarhossain23@gamil.comm>
2022-10-08 00:19:55 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
+
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#3346d3",
+          secondary: "#0287D0",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
