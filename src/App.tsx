35f6a10aa93836503428dac0ccf3b5604b0a0d0b
shariarhossain23e <shariarhossain23@gamil.comm>
2022-10-08 18:04:10 +0600

    add pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home";
+import Fotter from "./shared/Fotter";
 import SingleMovieCard from "./SIngleMovieCard/SingleMovieCard";
 
 function App() {
   return (
-    <div className="px-12">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/movie/:id" element={<SingleMovieCard />}></Route>
-      </Routes>
+    <div>
+      <div className="px-12 mb-6">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/movie/:id" element={<SingleMovieCard />}></Route>
+        </Routes>
+      </div>
+      <Fotter />
     </div>
   );
 }
--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -7,17 +7,40 @@ import MovieCard from "../MovieCard/MovieCard";
 const Movie = () => {
   const [movies, setMovies] = useState([]);
 
+  const [totalpage, setTotalPage] = useState(null);
+  const [page, setPage] = useState(1);
+
   const { data, isLoading, refetch } = useQuery("movie", () => {
     axios
-      .get("https://movie-task.vercel.app/api/popular?page=1")
+      .get(`https://movie-task.vercel.app/api/popular?page=1`)
       .then((res) => {
         setMovies(res.data.data.results);
       });
   });
+  const handleNext = () => {
+    setPage(page + 1);
+    axios
+      .get(`https://movie-task.vercel.app/api/popular?page=${page + 1}`)
+      .then((res) => {
+        setMovies(res.data.data.results);
+      });
+
+    console.log(page);
+  };
+  const handleBack = () => {
+    setPage(page - 1);
+    axios
+      .get(`https://movie-task.vercel.app/api/popular?page=${page}`)
+      .then((res) => {
+        setMovies(res.data.data.results);
+      });
+  };
 
   if (isLoading) {
     return <Loading />;
   }
+
+  console.log(page);
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     const search =
@@ -43,6 +66,52 @@ const Movie = () => {
         {movies
           ? movies.map((movie) => <MovieCard movie={movie}></MovieCard>)
           : "no result found"}
+      </div>
+      {movies.length === 0 && (
+        <p className="text-4xl text-center mt-[100px]">No Result Found</p>
+      )}
+      <div className="text-right">
+        {page !== 1 ? (
+          <button
+            onClick={handleBack}
+            className="btn btn-sm mr-6 relative top-1"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-arrow-left mr-2"
+              viewBox="0 0 16 16"
+            >
+              {" "}
+              <path
+                fill-rule="evenodd"
+                d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+              />{" "}
+            </svg>
+            Previous
+          </button>
+        ) : (
+          ""
+        )}
+        <button onClick={handleNext} className="btn btn-sm btn-primary">
+          Next
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-arrow-right ml-2"
+            viewBox="0 0 16 16"
+          >
+            {" "}
+            <path
+              fill-rule="evenodd"
+              d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+            />{" "}
+          </svg>
+        </button>
       </div>
     </div>
   );
