c99a7f0a90ea1b433dae1865f77b1a75468b8a79
shariarhossain23e <shariarhossain23@gamil.comm>
2022-10-08 15:41:28 +0600

    added serch movie

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -6,9 +6,7 @@ import MovieCard from "../MovieCard/MovieCard";
 
 const Movie = () => {
   const [movies, setMovies] = useState([]);
-  const [movieId, setMovieId] = useState({
-    id: "",
-  });
+
   const { data, isLoading, refetch } = useQuery("movie", () => {
     axios
       .get("https://movie-task.vercel.app/api/popular?page=1")
@@ -21,12 +19,30 @@ const Movie = () => {
     return <Loading />;
   }
 
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const search =
+      event.target.value.charAt(0).toUpperCase() + event.target.value.slice(1);
+    axios
+      .get(`https://movie-task.vercel.app/api/search?page=1&query=${search}`)
+      .then((res) => {
+        setMovies(res.data.data.results);
+      });
+  };
+
   return (
     <div className="">
+      <div className="mx-auto text-center mt-10">
+        <input
+          type="text"
+          placeholder="Search Movie"
+          className="input input-bordered w-full max-w-xs mx-auto"
+          onChange={(event) => handleSearch(event)}
+        />
+      </div>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
-        {movies.map((movie) => (
-          <MovieCard movie={movie}></MovieCard>
-        ))}
+        {movies
+          ? movies.map((movie) => <MovieCard movie={movie}></MovieCard>)
+          : "no result found"}
       </div>
     </div>
   );
